c6ca365fff46eb91265eb6d75397a5e8a9023e6b
Jackson <jbierfeldt@gmail.com>
2015-05-27 13:50:27 -0500

    Changed URL configuration

--- src/humorproject/urls.py
+++ src/humorproject/urls.py
@@ -5,9 +5,8 @@ from jokes.views import TestView, RandomJokeDetailView, JokeDetailView, JokeCrea
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', RandomJokeDetailView.as_view(), name='home'),
+    url(r'^$', RandomizedStudy.as_view(), name='survey'),
     url(r'^joke/(?P<joke_id>\d+)/$', JokeDetailView.as_view(), name='joke_detail'),
     url(r'^joke/(?P<joke_id>\d+)/rate/$', JokeRatingCreateView.as_view(), name='jokerating_create'),
     url(r'^joke/create/$', JokeCreateView.as_view(), name='joke_create'),
-    url(r'^survey/$', RandomizedStudy.as_view(), name='survey'),
 ]
