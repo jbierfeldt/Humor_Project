595eebe6bf2fe2d888821cb17dae4908e838fb9e
Jackson <jbierfeldt@gmail.com>
2015-06-08 10:44:12 -0500

    Added import_export to Admin

--- src/humorproject/settings/common.py
+++ src/humorproject/settings/common.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jokes',
+    'import_export',
 )
 
 MIDDLEWARE_CLASSES = (
--- src/jokes/admin.py
+++ src/jokes/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 from jokes.models import Joke, JokeRating
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-class JokeAdmin(admin.ModelAdmin):
+class JokeResource(resources.ModelResource):
+
+	class Meta:
+		model = Joke
+
+class JokeAdmin(ImportExportModelAdmin):
     list_display = ('text', 'approved', 'get_avg_humor_score', 'get_avg_taboo_score')
+    resource_class = JokeResource
     pass
-class JokeRatingAdmin(admin.ModelAdmin):
+
+class JokeRatingResource(resources.ModelResource):
+
+	class Meta:
+		model = JokeRating
+
+class JokeRatingAdmin(ImportExportModelAdmin):
     list_display = ('joke', 'humor_score', 'taboo_score')
+    resource_class = JokeRatingResource
     pass
+
+
+
 admin.site.register(Joke, JokeAdmin)
 admin.site.register(JokeRating, JokeRatingAdmin)
