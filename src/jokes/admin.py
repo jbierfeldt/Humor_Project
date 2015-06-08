9980556b72dcaab1c81da90bff40e81de05da785
Jackson <jbierfeldt@gmail.com>
2015-06-08 10:44:54 -0500

    Changed joke rating

--- src/jokes/admin.py
+++ src/jokes/admin.py
@@ -5,7 +5,7 @@ class JokeAdmin(admin.ModelAdmin):
     list_display = ('text', 'approved', 'get_avg_humor_score', 'get_avg_taboo_score', 'get_avg_taboo_rating_score')
     pass
 class JokeRatingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'humor_score', 'taboo_score', 'taboo_rating_score')
+    list_display = ('joke', 'humor_score', 'taboo_score', 'taboo_rating_score')
     pass
 admin.site.register(Joke, JokeAdmin)
 admin.site.register(JokeRating, JokeRatingAdmin)
